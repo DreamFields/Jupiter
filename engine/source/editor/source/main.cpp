3eb2b41c2d0f8ee5079de382a0af731e832ac8fc
Anastasiawangyx <1192146619@qq.com>
2023-11-09 16:43:48 +0800

    feat: 编译dcmtk并配置到项目中，配置启动项目

--- engine/source/editor/source/main.cpp
+++ engine/source/editor/source/main.cpp
@@ -5,7 +5,37 @@
 #include "runtime/engine.h"
 #include "editor/include/editor.h"
 
+// #include <dcmtk/dcmdata/dctk.h>
+#include "dcmtk/config/osconfig.h"
+#include "dcmtk/dcmdata/dctk.h"
+
 int main(int argc, char** argv) {
+    // DICOM解析
+    DcmFileFormat fileformat;
+    OFCondition status = fileformat.loadFile("C:\\Users\\Dream\\Documents\\00.Dicom\\ede6fe9eda6e44a98b3ad20da6f9116a Anonymized29\\Unknown Study\\CT Head 5.0000\\CT000000.dcm");
+    DcmDataset* dataset = fileformat.getDataset();
+    if (!status.good())
+    {
+        std::cout << "Load Dimcom File Error: " << status.text() << std::endl;
+        return -1;
+    }
+    OFString PatientName;
+    status = fileformat.getDataset()->findAndGetOFString(DCM_PatientName, PatientName);
+    if (status.good())
+    {
+        std::cout << "Get PatientName:" << PatientName << std::endl;
+    }
+    else
+    {
+        std::cout << "Get PatientName Error:" << status.text() << std::endl;
+        return -1;
+    }
+    // 获取图像像素数据
+    const Uint16 * pixelData = NULL;
+    dataset->findAndGetUint16Array(DCM_PixelData, pixelData);
+
+
+
     // 获得可执行文件名称与config文件目录
     std::filesystem::path executable_path(argv[0]);
     std::filesystem::path config_file_path = executable_path.parent_path() / "";
